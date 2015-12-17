6eea196adc24d571c9bc12c358df4972303dafba
Marton Bartal <bmmarci@gmail.com>
2015-12-17 21:59:33 +0100

    Starter UI.

--- lostnfound/client/js/main.js
+++ lostnfound/client/js/main.js
@@ -2,17 +2,30 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.lost_main.helpers({
+    //counter: function () {
+    //  return Session.get('counter');
+    //}
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.lost_main.events({
+    //'click button': function () {
+    //  // increment the counter when button is clicked
+    //  Session.set('counter', Session.get('counter') + 1);
+    //}
+  });
+
+  Template.lost_found.helpers({
+    //counter: function () {
+    //  return Session.get('counter');
+    //}
+  });
+
+  Template.lost_found.events({
+    //'click button': function () {
+    //  // increment the counter when button is clicked
+    //  Session.set('counter', Session.get('counter') + 1);
+    //}
   });
 }
 
