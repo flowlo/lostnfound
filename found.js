12cd39f607e8b7808983a482fdb11aea8c8559f7
Niki Laessig <niki.laessig@gmail.com>
2015-12-18 23:45:11 +0100

    fixed Listener bug

--- found.js
+++ found.js
@@ -21,6 +21,7 @@ function addQuestion(){
       return;
     }
 
+    validationQuestions++;
     var validationContainer = document.getElementById('validationQuestions');
 
     var row = document.createElement('div');
@@ -31,6 +32,7 @@ function addQuestion(){
     question.id="q"+validationQuestions;
     question.type="text";
     question.className += 'form-control';
+    question.addEventListener('change',validateQuestions, false);
     question.placeholder="Ask something only the owner knows. E.g. ID-number in case of a lost ID.";
 
     row.appendChild(question);
@@ -39,12 +41,16 @@ function addQuestion(){
     answer.id="a"+validationQuestions;
     answer.className += 'form-control';
     answer.placeholder="Answer to the validation Question.";
+    answer.addEventListener('change',validateQuestions, false);
     row.appendChild(answer);
+
+    document.getElementById('add').classList.add('hide');
 }
 
 
 function validateQuestions(){
   var addButton = document.getElementById('add');
+  console.log(validationQuestions);
 
   if(document.getElementById('q'+validationQuestions).value===''|| document.getElementById('a'+validationQuestions).value===''){
       addButton.classList.add('hide');
