149ac0d12548a5f5665e324d7131226fd4606a1a
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2015-12-18 17:47:55 +0100

    Dynamically render item list

--- lostnfound.js
+++ lostnfound.js
@@ -31,3 +31,50 @@ function chooser(hash) {
 	choose.style.display = 'block';
 	search.style.display = 'none';
 }
+
+var items = [
+	item('Braune Geldbörse (incl. eCard und Führerschein) leider ohne Bargeld', ['In welchem Jahr wurde der Führerschein ausgestellt?', 'Wie lautet die Sozialversicherungsnummer auf der E-Card?'], ['123456', '123456'], 'Bitte schreib\' mir eine E-Mail an finder@example.com! Bin erst wieder ab 6. Jänner in Wien!', '2015-12-17'),
+	item('Olivgrüne Jacke', ['Von welcher Marke ist die Jacke?'], ['DKNY'], 'Bin unter \'Die Finderin\' auf Facebook, einfach anschreiben!', '2015-12-18', 'http://lorempixel.com/100/100/abstract/'),
+
+]
+
+function item(description, questions, answers, contact, date, picture) {
+	return {
+		description: description,
+		questions: questions,
+		answers: answers,
+		contact: contact,
+		date: date,
+		picture: picture
+	};
+}
+
+function renderItems() {
+	var parent = document.getElementById('items');
+	for (var i = 0; i < items.length; i++) {
+		parent.appendChild(itemNode(items[i]));
+	}
+}
+
+function itemNode(item) {
+	var div = document.createElement('div');
+	div.className = 'item';
+	div.dataset.item = JSON.stringify(item);
+
+	div.appendChild(paragraph(item.description));
+	div.appendChild(paragraph(item.date));
+
+	if (item.picture) {
+		var img = document.createElement('img');
+		img.src = item.picture;
+		div.appendChild(img);
+	}
+
+	return div;
+}
+
+function paragraph(textContent) {
+	var node = document.createElement('p');
+	node.textContent = textContent;
+	return node;
+}
